51000173f231fd4cf36dae8d477823e051d40160
노은지 <77742161+bbbbeee11a@users.noreply.github.com>
2026-04-24 18:12:00 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,28 @@
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         d = {}
+#         result = []
+
+#         for s in strs:
+#             sorted_str = ''.join(sorted(s))
+#             if sorted_str in d:
+#                 d[sorted_str].append(s)
+#             else:
+#                 d[sorted_str] = [s]
+
+#         for key in d.keys():
+#             result.append(d[key])
+        
+#         return result
+        
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = {}
-        result = []
+        d = defaultdict(list)
 
         for s in strs:
-            sorted_str = ''.join(sorted(s))
-            if sorted_str in d:
-                d[sorted_str].append(s)
-            else:
-                d[sorted_str] = [s]
+            key = ''.join(sorted(s))
+            d[key].append(s)
 
-        for key in d.keys():
-            result.append(d[key])
-        
-        return result
-        
+        return list(d.values())
