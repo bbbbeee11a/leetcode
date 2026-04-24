95e132adb08129a95b5b3018308b3623ad50f0e0
노은지 <77742161+bbbbeee11a@users.noreply.github.com>
2026-04-24 15:51:03 +0900

    Time: 0 ms (100%), Space: 20.4 MB (44.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        d = {}  # 값 -> 인덱스
+
+        for idx, num in enumerate(nums):
+            complement = target - num
+
+            if complement in d:
+                return [d[complement], idx]
+            
+            else:
+                d[num] = idx
